a8195a25e0aadb23a60c9a1aa886a08f7fea4610
Siriusb24 <Siriusb2324@gmail.com>
2021-09-29 23:33:12 +0300

    summary

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+import galleryTamplate from './templates/countriesTemplate.hbs'
+import refs from './js/refs'
+import ApiService from './js/apiService'
+
+import { alert, error, defaultModules } from '@pnotify/core/dist/PNotify.js'
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js'
+import '@pnotify/core/dist/PNotify.css'
+import '@pnotify/core/dist/BrightTheme.css'
+defaultModules.set(PNotifyMobile, {})
+
+const newApiService = new ApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e){
+e.preventDefault();
+clearGallery()
+newApiService.query = e.currentTarget.elements.query.value;
+newApiService.resetPage();
+newApiService.fetchGallery().then(createGallery);
+}
+
+function createGallery(data){
+refs.gallery.insertAdjacentHTML("beforeend", galleryTamplate(data))
+}
+
+function clearGallery(){
+    refs.gallery.innerHTML=''  
+}
+
---
+++ src/js/apiService.js
@@ -0,0 +1,30 @@
+export default class ApiService{
+    constructor(){
+        this.searchQuery =""
+        this.page = 1;
+    }
+
+    fetchGallery(){
+        const BASE_URL = `https://pixabay.com/api/?key=23619099-a04eeaf87ea4b7be140530c42&q=${this.searchQuery}&per_page=12&page=${this.page}`
+
+        return fetch(BASE_URL).then(res => res.json()).then(data => {
+            this.incrementPage();
+            return data});
+    }
+
+    incrementPage(){
+        this.page+=1;
+    }
+
+    resetPage(){
+        this.page=1;
+    }
+
+    get query(){
+        return this.searchQuery
+    }
+
+    set query (newQuery){
+        this.searchQuery = newQuery 
+    }
+}
